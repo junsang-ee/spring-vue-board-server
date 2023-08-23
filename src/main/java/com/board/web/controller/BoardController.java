0e6ffa9dea78a77b6a5aca3d2c272bd235459942
junsang-ee <dkwnstkddk@gmail.com>
2023-08-24 01:17:28 +0900

    Add test http method Add RequestMapping in boardController

--- src/main/java/com/board/web/controller/BoardController.java
+++ src/main/java/com/board/web/controller/BoardController.java
@@ -1,8 +1,17 @@
 package com.board.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RequestMapping("/api/board")
 @RestController
 public class BoardController {
 
+    @GetMapping
+    public void test() {
+        log.info("test http method");
+    }
 }
