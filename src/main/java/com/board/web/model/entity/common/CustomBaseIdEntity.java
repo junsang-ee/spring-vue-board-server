28f0c78dc9183837523b06ff52c0fb0bd4785194
junsang-ee <dkwnstkddk@gmail.com>
2023-08-25 19:43:34 +0900

    Create CustomBaseIdGenerator config and baaseIdEntity

---
+++ src/main/java/com/board/web/model/entity/common/CustomBaseIdEntity.java
@@ -0,0 +1,27 @@
+package com.board.web.model.entity.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+public class CustomBaseIdEntity implements Persistable<String> {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "idGenerator")
+    @GenericGenerator(name="idGenerator", strategy = "com.board.web.config.CustomBaseIdGenerator")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String id;
+
+    @JsonIgnore
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+}
