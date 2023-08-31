61114f0c98bb5c6009d81d294c7aeaaccca3dadc
junsang-ee <dkwnstkddk@gmail.com>
2023-08-31 17:47:04 +0900

    Create PostActivationStatus enum and Add status member In PostEntity

---
+++ src/main/java/com/board/web/constants/PostActivationStatus.java
@@ -0,0 +1,5 @@
+package com.board.web.constants;
+
+public enum PostActivationStatus {
+    NORMAL, ERASED, DISABLED
+}
--- src/main/java/com/board/web/model/entity/PostEntity.java
+++ src/main/java/com/board/web/model/entity/PostEntity.java
@@ -1,11 +1,11 @@
 package com.board.web.model.entity;
 
+import com.board.web.constants.BoardActivationStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -18,4 +18,11 @@ public class PostEntity extends CreationTimestampEntity{
 
     private String content;
 
+    @Enumerated(EnumType.STRING)
+    private BoardActivationStatus status;
+
+    @PrePersist
+    public void onPrevisionPersist() {
+        this.status = BoardActivationStatus.NORMAL;
+    }
 }
