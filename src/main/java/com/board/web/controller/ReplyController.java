af482b49a159d4bb7ddce230768fbd8e547f5e43
DESKTOP-5GJCEAC\Administrator <dkwnstkdk@gmail.com>
2023-09-19 23:31:39 +0900

    Create ReplyController

---
+++ src/main/java/com/board/web/controller/ReplyController.java
@@ -0,0 +1,14 @@
+package com.board.web.controller;
+
+
+import com.board.web.model.entity.ReplyEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/reply")
+@RestController
+public class ReplyController {
+}
