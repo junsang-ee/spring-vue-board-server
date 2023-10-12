74237080244fd337bd5ec34eca88f37d0306a962
junsang-ee <dkwnstkddk@gmail.com>
2023-10-12 22:44:38 +0900

    Create auth, user Controller

---
+++ src/main/java/com/board/web/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.board.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/auth")
+@RestController
+public class AuthController {
+}
---
+++ src/main/java/com/board/web/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.board.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/user")
+@RestController
+public class UserController {
+
+}
